655a8184f2164f55ff04a57be9e9f2403a75d9e6
guvenc <guvenckazanci@yahoo.com>
2020-08-11 23:42:32 +0300

    updated getter and setter methods

--- backend/src/main/java/ecommerce/app/backend/bigcommerce/order/BCOrder.java
+++ backend/src/main/java/ecommerce/app/backend/bigcommerce/order/BCOrder.java
@@ -1,51 +1,24 @@
 package ecommerce.app.backend.bigcommerce.order;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
 public class BCOrder {
 
+    @Getter @Setter
     private String id;
 
+    @Getter @Setter
     @JsonProperty("date_modified")
     private Date modifiedDate;
 
+    @Getter @Setter
     private String status;
 
+    @Getter @Setter
     @JsonProperty("total_inc_tax")
     private Float totalIncludingTax;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Date getModifiedDate() {
-        return modifiedDate;
-    }
-
-    public void setModifiedDate(Date modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
-
-    public Float getTotalIncludingTax() {
-        return totalIncludingTax;
-    }
-
-    public void setTotalIncludingTax(Float totalIncludingTax) {
-        this.totalIncludingTax = totalIncludingTax;
-    }
-
 }
--- backend/src/main/java/ecommerce/app/backend/bigcommerce/order/BCOrderProduct.java
+++ backend/src/main/java/ecommerce/app/backend/bigcommerce/order/BCOrderProduct.java
@@ -1,35 +1,17 @@
 package ecommerce.app.backend.bigcommerce.order;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 public class BCOrderProduct {
+    @Getter @Setter
     @JsonProperty("product_id")
     private String productId;
 
+    @Getter @Setter
     private String sku;
+
+    @Getter @Setter
     private Integer quantity;
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getSku() {
-        return sku;
-    }
-
-    public void setSku(String sku) {
-        this.sku = sku;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
 }
